a8819ca738f27b4d81c6c9392a37733423bde0f4
noormalfaz <noorm4210@gmail.com>
2023-11-09 18:17:40 +0700

    Isi data tabel

---
+++ isi-data-tabel-kategori-barang.sql
@@ -0,0 +1,11 @@
+INSERT INTO kategori_barang (nama_kategori_barang, stok) VALUES
+("Pakaian", 100),
+("Celana", 200),
+("Jaket", 300),
+("Kemeja", 400),
+("Topi", 500),
+("Sepatu", 400),
+("Kaus kaki", 300),
+("Sandal", 250),
+("Sweater", 150),
+("Kacamata", 50);
---
+++ isi-data-tabel-pelanggan.sql
@@ -0,0 +1,12 @@
+INSERT INTO pelanggan (nama, no_hp) VALUES
+("Ilham", 0812345678),
+("Fahmi", 0823456789),
+("Pramudya", 0834567890),
+("Aldi", 0845678901),
+("Rohman", 0856789012),
+("Vikry", 0867890123),
+("Helmi", 0878901234),
+("Syehan", 0889012345),
+("Elsa", 0890123456),
+("Tia", 0801234567),
+("Adila", 0809876543);
